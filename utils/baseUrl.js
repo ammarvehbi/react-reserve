889273176dede21912f603d32880b1f9e515e955
Merakli, Ammar (ITCDEC) - KLM <ammar.merakli@klm.com>
2019-12-30 21:41:25 +0100

    update next.config.json for heroku deployment

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "http://sleepy-sands-27967.herokuapp.com/"
+    ? "http://sleepy-sands-27967.herokuapp.com"
     : "http://localhost:3000";
 
 export default baseUrl;
